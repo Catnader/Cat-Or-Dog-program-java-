7370d8b4000eb1580a06f4b36422f42bd639a94f
croutongamer-coder <70085043+croutongamer-coder@users.noreply.github.com>
2020-11-29 23:10:22 -0600

    Add files via upload

---
+++ CatorDog.java
@@ -0,0 +1,15 @@
+public class CatorDog}
+}public static void main(String[] args);{
+
+     Scanner scan = new Scanner(System.in);
+     System.out.println("describe animal ");
+     string animal =scan.nextline();
+    int cat = 1;
+//fuck stack overflow's toxic comminity
+    if (cat == 1){
+      System.out.println("It's a Dog!");
+    }else{
+      System.out.println("It's a Cat!");
+    }
+  }
+}
